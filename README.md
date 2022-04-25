d1b1df3bf623a47fec1a2c692657403d4f42f3ff
The Shell Mechanic <theshellmechanic@gmail.com>
2022-04-25 18:21:52 +0100

    Upload Initial Release Code

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# ItemJumbler
+# NFT Juggler
+NFT images created using NFT art engines are generated in a sequential pattern, and with this comes the issue of neighbouring images to look very similar to each other. Removing the chance of them looking the same allows for what NFT's are about, randomness.
+
+The randomness in the images can come in the smart contract, but creating a smart contract with more functions than what is necessary creates higher gas fees. This keeps it simple and cheap.
+
+## Installation
+
+```sh
+git clone https://github.com/TheShellMechanic/ItemJuggler.git
+```
+```sh
+python3 -m pip install -r requirements.txt
+```
+
+## Prerequisites
+First you need to populate a folder named build with all of the assets that need to be converted. This includes the untouched, newly rendered, .png files and .json files. (If the images have a different file name edit lines 30, 31 and 44 to fit your needs)
+
+You as the user need to edit the following to fit your needs:
+ - AMOUNT
+ - IPFS
+ - NAME
+
+
+## Note
+
+Most code was taken from:
+https://github.com/kamilsadik/nft_shuffler
+but due to it being a Juniper file, there was no point in forking and editting it.
---
+++ art.py
@@ -0,0 +1,53 @@
+import os
+import json
+import random
+import shutil
+
+AMOUNT = 432
+SOURCE = "build"
+
+IPFS = "ipfs://PATH/"
+OUTCOME = "shuffled_assets"
+NAME = "Collection Name"
+pre_selected_src_indices = []
+
+if not os.path.exists('shuffled_assets/images'):
+    os.makedirs('shuffled_assets/images')
+
+if not os.path.exists('shuffled_assets/json'):
+    os.makedirs('shuffled_assets/json')
+
+src_indices = [n for n in range(0,AMOUNT) if n not in pre_selected_src_indices]
+src_indices = pre_selected_src_indices + src_indices
+
+pre_selected_dest_indices = [n for n in range(0, len(pre_selected_src_indices))]
+dest_indices = [n for n in range(len(pre_selected_src_indices),AMOUNT)]
+random.shuffle(dest_indices)
+dest_indices = pre_selected_dest_indices + dest_indices
+
+for index in range(0, AMOUNT):
+    try:        
+        src_img = f"{SOURCE}/{src_indices[index]}.png"
+        dest_img = f"{OUTCOME}/images/{dest_indices[index]}.png"
+        shutil.copy(src_img, dest_img)
+        
+        src_json = f"{SOURCE}/{src_indices[index]}.json"
+        dest_json = f"{OUTCOME}/json/{dest_indices[index]}.json"
+        shutil.copy(src_json, dest_json)
+        
+        file = open(f"{OUTCOME}/json/{dest_indices[index]}.json", "r")
+        json_object = json.load(file)
+        file.close()
+        
+        json_object["name"] = f"{NAME} #{dest_indices[index]}"
+        json_object["description"] = f"PUT IN YOUR DESCRIPTION"
+        json_object["image"] = f"{IPFS}{dest_indices[index]}.png"
+        json_object["edition"] = dest_indices[index]
+        
+        file = open(f"{OUTCOME}/json/{dest_indices[index]}.json", "w")
+        json.dump(json_object, file, ensure_ascii=False, indent=4)
+        file.close()
+        print("done")
+    except Exception as e:
+        print(e)
+        pass
